3473bf3b21093679e04b3fa1eb8e5bd5e43c6aff
Brad Rubendall <luyufeng666@foxmail.com>
2024-07-12 14:09:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 var address = {
 				"city_level":2,
 				"country":"美国",
-				"town":"铁心桥街道",
+				"town":"美国",
 				"distance":"",
-				"city":"阿富汗",
+				"city":"美国",
 				"adcode":"320114",
 				"country_code_iso":"CHN",
 				"country_code_iso2":"CN",
 				"country_code":0,
 				"town_code":"320114006",
-				"province":"阿富汗",
-				"street":"阿富汗",
-				"district":"阿富汗",
+				"province":"美国",
+				"street":"美国",
+				"district":"美国",
 				"street_number":"",
 				"direction":""
 			};
