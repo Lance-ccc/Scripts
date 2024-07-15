a62c294919a57e7902e0903d7514af6526ee9a0d
Brad Rubendall <luyufeng666@foxmail.com>
2024-07-15 21:56:32 +0800

    更新 request.js

--- request.js
+++ request.js
@@ -7,7 +7,7 @@ let method = req.method;
 $.log("method type" + method);
 
 if(method === 'POST'){
-    const location = {"location":"31.977030865550024,118.7786218148147"};
+    const location = {"location":"31.977030865550024,118.7786218148143"};
     req.body = JSON.stringify(location);
     $.log("修改后的："+ req.body);
 }
