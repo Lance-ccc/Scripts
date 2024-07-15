575c79e5ee66e1cda22459e417779ab086fa1bc8
Brad Rubendall <luyufeng666@foxmail.com>
2024-07-15 19:38:49 +0800

    Update request.js

--- request.js
+++ request.js
@@ -7,7 +7,7 @@ let method = req.method;
 $.log("method type" + method);
 
 if(method === 'POST'){
-    const location = {"location":"31.977030865550024,118.7786218148147"};
+    const location = {"location":"31.977030865550031,118.7786218148150"};
     req.body = JSON.stringify(location);
     $.log(JSON.stringify(req));
 }
